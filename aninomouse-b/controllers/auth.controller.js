737497371357d6e7a2af103c5ab6c1fdbf07e890
shiva <shivangishree020@gmail.com>
2025-08-12 20:54:23 +0530

    login signup backend controller, routes added

---
+++ aninomouse-b/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+export const signup = async (req, res) => {
+
+}
+
+export const login = async (req, res) => {
+
+}
--- aninomouse-b/index.js
+++ aninomouse-b/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import connectDB from './config/db';
+import connectDB from './config/db.js';
+import authRoutes from './routes/auth.routes.js'
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -11,7 +12,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', require('./routes/auth.routes'));
+app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
